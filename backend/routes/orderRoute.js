9e911d90927e62f8d5bef17ed255f9fc9fc38690
abhishekhojha <abhishekhojha45@gmail.com>
2025-03-29 00:43:50 +0530

    feat: add endpoint to retrieve purchased courses for a user and user profile details

--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -8,6 +8,7 @@ const {
 const hasRole = require("../middleware/Auth");
 
 const protect = require("../middleware/authMiddleware");
+const Order = require("../models/Order")
 
 // Route to create order
 router.post("/create-order", protect, createOrder);
@@ -15,5 +16,23 @@ router.post("/create-order", protect, createOrder);
 // Route to verify payment
 router.post("/verify-payment", verifyPayment);
 router.get("/", hasRole, getAllOrders);
+// Get purchased courses for a user
+router.get("/purchased-courses",protect ,async (req, res) => {
+  try {
+    const userId = req.user.id;
+
+    const orders = await Order.find({ user: userId, status: "paid" })
+      .populate("course")
+      .sort({ paymentDate: -1 });
+
+    if (!orders.length) {
+      return res.status(404).json({ message: "No purchased courses found" });
+    }
+
+    res.status(200).json(orders);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
 
 module.exports = router;
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -107,7 +107,17 @@ router.get("/profile", protect, async (req, res) => {
     res.status(500).json({ error: err.message });
   }
 });
-
+router.get("/me", protect, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select("-password"); // Exclude password
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
 // Get All Users
 router.get("/all", hasRole, async (req, res) => {
   try {
